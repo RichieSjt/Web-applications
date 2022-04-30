fee0d818d7abdea48788ee36fa4c7b2d41352513
Ricardo Juárez Tepos <45005831+RichieSjt@users.noreply.github.com>
2022-04-30 15:08:56 -0500

    React exercises

---
+++ react-example/example-2/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import HelloWorld from './components/HelloWorld';
+
+const appContainer = document.getElementById('react-app');
+ReactDOM.render(<HelloWorld />, appContainer);
